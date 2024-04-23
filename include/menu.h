97dec42a614f1a2b35a00fda1616af85f4d9aa60
j03m4r <joeemarr12345@gmail.com>
2024-04-23 11:36:57 +0200

    Added doxygen/html and doxygen/latex to .gitignore. Documented menu.h.

--- include/menu.h
+++ include/menu.h
@@ -4,6 +4,16 @@
 #include <iostream>
 #include "scene.h"
 
+/**
+ * @brief Class that implements scene to create the menu scene.
+ * 
+ * Entry scene to ConnectN. Allows the user to navigate to three different scenes: Game, Settings, and Credits
+ * 
+ * @see Scene
+ * @see Game
+ * @see Settings
+ * @see Credits
+ */
 class Menu : public Scene {
 private:
     std::string PLAY_GAME = "1";
